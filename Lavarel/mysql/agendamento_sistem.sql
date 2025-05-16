cb33962e35d8665e67e3167dc2d98033c7085b70
Gabrielseilhe <168229694+gaberilseilhe@users.noreply.github.com>
2025-05-16 17:32:19 -0300

    Add files via upload

--- Lavarel/mysql/agendamento_sistem.sql
+++ Lavarel/mysql/agendamento_sistem.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 27/04/2025 às 03:02
+-- Tempo de geração: 16/05/2025 às 22:29
 -- Versão do servidor: 10.4.32-MariaDB
 -- Versão do PHP: 8.2.12
 
@@ -24,6 +24,27 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Estrutura para tabela `agenda`
+--
+
+CREATE TABLE `agenda` (
+  `id_agenda` int(11) NOT NULL,
+  `id_usuario` int(11) NOT NULL,
+  `dia_da_semana` varchar(64) NOT NULL,
+  `horario` time NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Despejando dados para a tabela `agenda`
+--
+
+INSERT INTO `agenda` (`id_agenda`, `id_usuario`, `dia_da_semana`, `horario`) VALUES
+(1, 1, 'segunda-feira', '10:00:00'),
+(2, 1, '1000-10-10', '12:00:00');
+
+-- --------------------------------------------------------
+
+--
 -- Estrutura para tabela `agendamento`
 --
 
@@ -40,27 +61,9 @@ CREATE TABLE `agendamento` (
 --
 
 INSERT INTO `agendamento` (`id_agendamento`, `id_usuario`, `id_servico`, `id_agenda`, `data`) VALUES
-(1, 1, 1, 1, '2025-04-01');
-
--- --------------------------------------------------------
-
---
--- Estrutura para tabela `agendas`
---
-
-CREATE TABLE `agendas` (
-  `id_agenda` int(11) NOT NULL,
-  `id_usuario` int(11) NOT NULL,
-  `dia_da_semana` varchar(64) NOT NULL,
-  `horario` time NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Despejando dados para a tabela `agendas`
---
-
-INSERT INTO `agendas` (`id_agenda`, `id_usuario`, `dia_da_semana`, `horario`) VALUES
-(1, 1, 'segunda-feira', '10:00:00');
+(1, 1, 1, 1, '2025-04-01'),
+(2, 1, 1, 1, '1200-12-23'),
+(3, 1, 1, 1, '1200-12-23');
 
 -- --------------------------------------------------------
 
@@ -78,7 +81,8 @@ CREATE TABLE `grupos` (
 --
 
 INSERT INTO `grupos` (`id_grupo`, `nome_grupo`) VALUES
-(1, 'admin');
+(1, 'admin'),
+(2, 'Admin');
 
 -- --------------------------------------------------------
 
@@ -97,7 +101,12 @@ CREATE TABLE `servicos` (
 --
 
 INSERT INTO `servicos` (`id_servicos`, `nome_servico`, `valor`) VALUES
-(1, 'corte', 50);
+(1, 'corte', 50),
+(2, 'corte', 60),
+(3, 'corte', 60),
+(4, 'corte', 60),
+(5, 'corte', 60),
+(6, 'barba', 50);
 
 -- --------------------------------------------------------
 
@@ -109,31 +118,39 @@ CREATE TABLE `usuarios` (
   `id_usuario` int(11) NOT NULL,
   `nome_usuario` varchar(128) NOT NULL,
   `data_nascimento` date NOT NULL,
-  `id_grupo` int(11) NOT NULL
+  `id_grupo` int(11) NOT NULL,
+  `updated_at` date NOT NULL DEFAULT current_timestamp(),
+  `created_at` date NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Despejando dados para a tabela `usuarios`
 --
 
-INSERT INTO `usuarios` (`id_usuario`, `nome_usuario`, `data_nascimento`, `id_grupo`) VALUES
-(1, 'jorge', '2025-04-01', 1);
+INSERT INTO `usuarios` (`id_usuario`, `nome_usuario`, `data_nascimento`, `id_grupo`, `updated_at`, `created_at`) VALUES
+(1, 'jorge', '2025-04-01', 1, '2025-05-16', '2025-05-16'),
+(2, 'gabe', '2004-12-12', 1, '2025-05-16', '2025-05-16'),
+(3, 'gabe', '2004-12-12', 1, '2025-05-16', '2025-05-16'),
+(4, 'jovel', '2141-01-11', 3, '2025-05-16', '2025-05-16'),
+(5, 'gabe', '2004-12-12', 1, '2025-05-16', '2025-05-16'),
+(6, 'gabe', '2004-12-12', 1, '2025-05-16', '2025-05-16'),
+(7, 'gabe', '2004-12-12', 1, '2025-05-16', '2025-05-16');
 
 --
 -- Índices para tabelas despejadas
 --
 
 --
+-- Índices de tabela `agenda`
+--
+ALTER TABLE `agenda`
+  ADD PRIMARY KEY (`id_agenda`);
+
+--
 -- Índices de tabela `agendamento`
 --
 ALTER TABLE `agendamento`
   ADD PRIMARY KEY (`id_agendamento`);
-
---
--- Índices de tabela `agendas`
---
-ALTER TABLE `agendas`
-  ADD PRIMARY KEY (`id_agenda`);
 
 --
 -- Índices de tabela `grupos`
@@ -152,6 +169,40 @@ ALTER TABLE `servicos`
 --
 ALTER TABLE `usuarios`
   ADD PRIMARY KEY (`id_usuario`);
+
+--
+-- AUTO_INCREMENT para tabelas despejadas
+--
+
+--
+-- AUTO_INCREMENT de tabela `agenda`
+--
+ALTER TABLE `agenda`
+  MODIFY `id_agenda` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT de tabela `agendamento`
+--
+ALTER TABLE `agendamento`
+  MODIFY `id_agendamento` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT de tabela `grupos`
+--
+ALTER TABLE `grupos`
+  MODIFY `id_grupo` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT de tabela `servicos`
+--
+ALTER TABLE `servicos`
+  MODIFY `id_servicos` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT de tabela `usuarios`
+--
+ALTER TABLE `usuarios`
+  MODIFY `id_usuario` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
