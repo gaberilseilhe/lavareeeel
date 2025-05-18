faebe97f04c331928cbb7e555ebd423b736f4162
Gabrielseilhe <168229694+gaberilseilhe@users.noreply.github.com>
2025-05-18 20:38:29 -0300

    Add files via upload

--- Lavarel/controller/UserController.php
+++ Lavarel/controller/UserController.php
@@ -6,16 +6,21 @@ use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
-use App\Models\User;
+use App\Models\User; // model do usuario
 use Illuminate\Http\Request;
 
 class UserController extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
 
-    public function createUsuario(Request $request)
-    {
-        $usuarioCriado = User::create([
+    public function createUsuario(Request $request) // (request necessario)
+    {   
+        
+      
+
+//variavel(pode ter qualquer nome) //Model usuario (User)
+        $usuarioCriado  = User::create([
+  //nome das colunas na tabela         e o que tem valor na url
             'nome_usuario' => $request->nome_usuario,
             'id_grupo' => $request->id_grupo,
             'data_nascimento' => $request->data_nascimento,
