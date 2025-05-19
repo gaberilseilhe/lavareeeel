f04bee4de36e1dd7e7a3bc201a5ebaadb5768aa3
Gabrielseilhe <168229694+gaberilseilhe@users.noreply.github.com>
2025-05-18 21:01:42 -0300

    Add files via upload

---
+++ Lavarel/web/web (1).php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\ServicosController;
+use App\Http\Controllers\AgendamentoController;
+use App\Http\Controllers\AgendaController;
+use App\Http\Controllers\GruposController;
+
+
+Route::get('usuarios', [UserController::class, 'getusuarios']);
+
+Route::get('servicos', [ServicosController::class, 'getservicos']);
+
+Route::get('agendamento', [AgendamentoController::class, 'getagendamento']);
+
+Route::get('agenda', [AgendaController::class, 'getagenda']);
+
+Route::get('grupos', [GruposController::class, 'getgrupos']);
+
+Route::get('/create-usuario', [//nome do que vai vim depois do '/' de agendamento_sistem na url
+    UserController::class,  // nome da classe
+    'createUsuario'//nome da função em UserController
+]); 
+Route::get('/create-servico', [
+    ServicosController::class,
+    'createServico'
+]);
+Route::get('/create-grupo', [
+    GruposController::class,
+    'createGrupo'
+]);
+Route::get('/create-agendamento', [
+    AgendamentoController::class,
+    'createAgendamento'
+]);
+Route::get('/create-agenda', [
+    AgendaController::class,
+    'createAgenda'
+]);
